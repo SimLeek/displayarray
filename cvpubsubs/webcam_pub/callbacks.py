4586e9743f6bcd007a5dc157b63d8a2152dbec09
SimLeek <josh.miklos@gmail.com>
2019-02-24 21:04:35 -0700

    callbacks: Added pytorch function display and pytorch conway life example. Still need to test coords.

--- cvpubsubs/webcam_pub/callbacks.py
+++ cvpubsubs/webcam_pub/callbacks.py
@@ -65,3 +65,78 @@ class function_display_callback(object):  # NOSONAR
 
     def __call__(self, *args, **kwargs):
         return self.inner_function(self, *args, **kwargs)
+
+
+class pytorch_function_display_callback(object):  # NOSONAR
+    def __init__(self, display_function, finish_function=None):
+        """Used for running arbitrary functions on pixels.
+
+        >>> import random
+        >>> import torch
+        >>> from cvpubsubs.webcam_pub import VideoHandlerThread
+        >>> img = np.zeros((300, 300, 3))
+        >>> def fun(array, coords, finished):
+        ...     rgb = torch.empty(array.shape).uniform_(0,1).type(torch.DoubleTensor).to(array.device)/200.0
+        ...     array[coords] = (array[coords] + rgb[coords])%1.0
+        >>> VideoHandlerThread(video_source=img, callbacks=pytorch_function_display_callback(fun)).display()
+
+        thanks: https://medium.com/@awildtaber/building-a-rendering-engine-in-tensorflow-262438b2e062
+
+        :param display_function:
+        :param finish_function:
+        """
+
+        import torch
+        from torch.autograd import Variable
+
+        self.looping = True
+        self.first_call = True
+
+        def _run_finisher(self, frame, finished, *args, **kwargs):
+            if not callable(finish_function):
+                WinCtrl.quit()
+            else:
+                finished = finish_function(frame, Ellipsis, finished, *args, **kwargs)
+                if finished:
+                    WinCtrl.quit()
+
+        def _setup(self, frame, cam_id, *args, **kwargs):
+
+            if "device" in kwargs:
+                self.device = torch.device(kwargs["device"])
+            else:
+                if torch.cuda.is_available():
+                    self.device = torch.device("cuda")
+                else:
+                    self.device = torch.device("cpu")
+
+            self.min_bounds = [0 for _ in frame.shape]
+            self.max_bounds = list(frame.shape)
+            grid_slices = [slice(self.min_bounds[d], self.max_bounds[d]) for d in range(len(frame.shape))]
+            self.space_grid = np.mgrid[grid_slices]
+            x_tens = torch.LongTensor(self.space_grid[0, ...]).to(self.device)
+            y_tens = torch.LongTensor(self.space_grid[1, ...]).to(self.device)
+            c_tens = torch.LongTensor(self.space_grid[2, ...]).to(self.device)
+            self.x = Variable(x_tens, requires_grad=False)
+            self.y = Variable(y_tens, requires_grad=False)
+            self.c = Variable(c_tens, requires_grad=False)
+
+        def _display_internal(self, frame, cam_id, *args, **kwargs):
+            finished = True
+            if self.first_call:
+                # return to display initial frame
+                _setup(self, frame, finished, *args, **kwargs)
+                self.first_call = False
+                return
+            if self.looping:
+                tor_frame = torch.from_numpy(frame).to(self.device)
+                finished = display_function(tor_frame, (self.x, self.y, self.c), finished, *args, **kwargs)
+                frame[...] = tor_frame.cpu().numpy()[...]
+            if finished:
+                self.looping = False
+                _run_finisher(self, frame, finished, *args, **kwargs)
+
+        self.inner_function = _display_internal
+
+    def __call__(self, *args, **kwargs):
+        return self.inner_function(self, *args, **kwargs)
