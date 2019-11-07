86e073faec9c20d21ef0dabcb5cbe2e940d3c691
simleek <josh.miklos@gmail.com>
2019-11-06 20:44:25 -0700

    Added rospy coroutines. Added command line interface.

--- displayarray/frame/frame_updater.py
+++ displayarray/frame/frame_updater.py
@@ -1,4 +1,5 @@
 import threading
+import asyncio
 from typing import Union, Tuple, Any, Callable, List, Optional, Dict
 
 import numpy as np
@@ -111,7 +112,7 @@ class FrameUpdater(threading.Thread):
             raise self.exception_raised
 
 
-def read_updates(
+async def read_updates(
     *vids,
     callbacks: Optional[
         Union[Dict[Any, FrameCallable], List[FrameCallable], FrameCallable]
@@ -158,7 +159,136 @@ def read_updates(
                 dict_was_updated = True
         if dict_was_updated or not blocking:
             yield vid_update_dict
+        await asyncio.sleep(0)
     for v in vid_names:
         subscriber_dictionary.stop_cam(v)
     for v in vid_threads:
         v.join()
+
+
+async def read_updates_zero_mq(
+    *topic_names,
+    address="tcp://127.0.0.1:5600",
+    flags=0,
+    copy=True,
+    track=False,
+    blocking=False,
+    end_callback: Callable[[Any], bool] = lambda: False,
+):
+    import zmq
+
+    ctx = zmq.Context()
+    s = ctx.socket(zmq.SUB)
+    s.connect(address)
+    if not blocking:
+        flags |= zmq.NOBLOCK
+
+    for topic in topic_names:
+        s.setsockopt(zmq.SUBSCRIBE, topic)
+    cb_val = False
+    while not cb_val:
+        try:
+            md = s.recv_json(flags=flags)
+            msg = s.recv(flags=flags, copy=copy, track=track)
+            buf = memoryview(msg)
+            arr = np.frombuffer(buf, dtype=md["dtype"])
+            arr.reshape(md["shape"])
+            name = md["name"]
+            cb_val = end_callback(md)
+            yield name, arr
+        except zmq.ZMQError as e:
+            if isinstance(e, zmq.Again):
+                pass  # no messages to receive
+            else:
+                raise e
+        finally:
+            await asyncio.sleep(0)
+
+
+async def read_updates_ros(
+    *topic_names,
+    dtypes=None,
+    listener_node_name=None,
+    poll_rate_hz=None,
+    end_callback: Callable[[Any], bool] = lambda: False,
+):
+    import rospy
+    from rospy.numpy_msg import numpy_msg
+    from rospy.client import _WFM
+    import std_msgs.msg
+    import random
+    import string
+
+    if dtypes is None:
+        raise ValueError(
+            "ROS cannot automatically determine the types of incoming numpy arrays. Please specify.\n"
+            "Options are: \n"
+            "\tfloat32, float64, bool, char, int16, "
+            "\tint32, int64, str, uint16, uint32, uint64, uint8"
+        )
+
+    if listener_node_name is None:
+        # https://stackoverflow.com/a/2257449
+        listener_node_name = "".join(
+            random.choices(string.ascii_uppercase + string.digits, k=8)
+        )
+
+    rospy.init_node(listener_node_name)
+
+    msg_types = [
+        {
+            np.float32: std_msgs.msg.Float32(),
+            np.float64: std_msgs.msg.Float64(),
+            np.bool: std_msgs.msg.Bool(),
+            np.char: std_msgs.msg.Char(),
+            np.int16: std_msgs.msg.Int16(),
+            np.int32: std_msgs.msg.Int32(),
+            np.int64: std_msgs.msg.Int64(),
+            np.str: std_msgs.msg.String(),
+            np.uint16: std_msgs.msg.UInt16(),
+            np.uint32: std_msgs.msg.UInt32(),
+            np.uint64: std_msgs.msg.UInt64(),
+            np.uint8: std_msgs.msg.UInt8(),
+            "float32": std_msgs.msg.Float32(),
+            "float64": std_msgs.msg.Float64(),
+            "bool": std_msgs.msg.Bool(),
+            "char": std_msgs.msg.Char(),
+            "int16": std_msgs.msg.Int16(),
+            "int32": std_msgs.msg.Int32(),
+            "int64": std_msgs.msg.Int64(),
+            "str": std_msgs.msg.String(),
+            "uint16": std_msgs.msg.UInt16(),
+            "uint32": std_msgs.msg.UInt32(),
+            "uint64": std_msgs.msg.UInt64(),
+            "uint8": std_msgs.msg.UInt8(),
+        }.get(
+            dtype, dtype
+        )  # allow users to use their own custom messages in numpy arrays
+        for dtype in dtypes
+    ]
+    s = None
+    cb_val = False
+    try:
+        wfms = {t: _WFM() for t in topic_names}
+        s = {
+            t: rospy.Subscriber(t, numpy_msg(msg_types[i]), wfms[t].cb)
+            for i, t in enumerate(topic_names)
+        }
+        while not cb_val:
+            while not rospy.core.is_shutdown():
+                if poll_rate_hz:
+                    await asyncio.sleep(1.0 / poll_rate_hz)
+                else:
+                    await asyncio.sleep(0)
+                for t, w in wfms.items():
+                    if w.msg is not None:
+                        yield t, w.msg
+                        cb_val = end_callback(w.msg)
+                        w.msg = None
+    except KeyboardInterrupt:
+        pass
+    finally:
+        if s is not None:
+            s.unregister()
+    if rospy.core.is_shutdown():
+        raise rospy.exceptions.ROSInterruptException("rospy shutdown")
