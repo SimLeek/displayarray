3a440a30d4f37baf65b1ba8c9361e9096d036e07
simleek <josh.miklos@gmail.com>
2020-04-07 22:08:15 -0700

    updated version

--- displayarray/__init__.py
+++ displayarray/__init__.py
@@ -4,7 +4,7 @@ Display any array, webcam, or video file.
 display is a function that displays these in their own windows.
 """
 
-__version__ = "0.7.2"
+__version__ = "0.7.3"
 
 from .window.subscriber_windows import display, breakpoint_display, read_updates
 from .frame.frame_publishing import publish_updates_zero_mq, publish_updates_ros
