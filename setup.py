a5575374af8b49ac975c549776c634b6e7a259f6
Josh Miklos <josh.miklos@gmail.com>
2020-07-04 20:39:16 -0700

    Updated to latest numpy to fix dependency error

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     install_requires=[
         "docopt==0.6.2",
         "localpubsub==0.0.4",
-        "numpy==1.16.1",
+        "numpy>=1.17.0",
         "opencv-python==4.*,>=4.0.0",
         "pyzmq==18.1.0",
     ],
